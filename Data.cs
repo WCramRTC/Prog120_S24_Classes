b4b88388a15b6ef4e27c39feb6e311c0dab03a42
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-05-23 17:27:43 -0700

    Potential Code

---
+++ Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace Prog120_S24_Classes
+{
+    internal class Data
+    {
+
+        // Uncomment from here----------------
+        public static FoodEntry[] ReadCsv(string relativePath)
+        {
+            string fullPath = Path.GetFullPath(relativePath);
+
+            using (var reader = new StreamReader(fullPath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = new List<FoodEntry>();
+                records.AddRange(csv.GetRecords<FoodEntry>());
+                return records.ToArray();
+            }
+        }
+
+        // To Here------------------
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public static void LoadData(T)
+        {
+            string relativePath = Path.Combine("Data", "myfile.csv");
+            var foodEntries = Data.ReadCsv(relativePath);
+        }
     } // class
+
+    public class FoodEntry
+    {
+        public string Name;
+        public double Calories;
+        public double Quantity;
+    }
 
 } // namespace
